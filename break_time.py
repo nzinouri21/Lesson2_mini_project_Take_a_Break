c5bee08b93e2edde4e44b6142106ba68a8d73d32
Nazanin <n.zinouri@gmail.com>
2017-12-17 12:48:24 -0500

    create a loop
    putting the open browser and wait into a loop

--- break_time.py
+++ break_time.py
@@ -1,8 +1,10 @@
 import webbrowser
 import time
+i=0
+while i<3:
+    #make the program wait for certain time
+    time.sleep(10)
 
-#make the program wait for certain time
-time.sleep(10)
-
-#Getting the program to open the URL to our favorite song!
-webbrowser.open("https://youtu.be/Sv6dMFF_yts?list=RDMMSv6dMFF_yts", new=0, autoraise=True)
+    #Getting the program to open the URL to our favorite song!
+    webbrowser.open("https://youtu.be/Sv6dMFF_yts?list=RDMMSv6dMFF_yts", new=0, autoraise=True)
+    i=i+1
